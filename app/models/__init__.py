0b65c7a2379b02aa13fbf97873969fad1cb6af65
Brendon <brendonnewton0@gmail.com>
2024-10-21 21:35:53 -0500

    creating models module

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+
+# Import other models as needed
+
+# For easy access to all models
+all_models = [
+    User,
+]
--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-
-@app.route("/")
-@app.route("/index")
-def index():
-    return "SQL more like sea quail amiright?"
