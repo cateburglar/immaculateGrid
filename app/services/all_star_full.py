887271347441f4c31deb214b5eec0837289bf324
Brendon <brendonnewton0@gmail.com>
2024-10-23 09:18:11 -0500

    adding AllStarFull data model

--- app/services/all_star_full.py
+++ app/services/all_star_full.py
@@ -1,21 +1,22 @@
 import csv
 
 from app.extensions import db
+from app.models import AllStarFull
 
 
-def process_hall_of_fame(file_path):
+def process_all_star_full(file_path):
     with open(file_path, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             # Assuming the CSV contains columns 'name' and 'value'
-            data_entry = DataModel.query.filter_by(name=row["name"]).first()
+            data_entry = AllStarFull.query.filter_by(name=row["name"]).first()
 
             if data_entry:
                 # Update the existing entry
                 data_entry.value = row["value"]
             else:
                 # Insert new entry if it doesn't exist
-                new_entry = DataModel(name=row["name"], value=row["value"])
+                new_entry = AllStarFull(name=row["name"], value=row["value"])
                 db.session.add(new_entry)
 
         db.session.commit()
