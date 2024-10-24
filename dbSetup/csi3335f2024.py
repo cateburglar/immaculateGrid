45773795a974cbd14b4b85dd20706312232998f5
Brendon <brendonnewton0@gmail.com>
2024-10-24 13:55:46 -0500

    updates now go through

--- dbSetup/csi3335f2024.py
+++ dbSetup/csi3335f2024.py
@@ -4,3 +4,10 @@ mysql = {
     "password": "password",
     "db": "seaquail",
 }
+
+baseballmysql = {
+    "host": "localhost",
+    "user": "root",
+    "password": "password",
+    "db": "baseball",
+}
--- dbSetup/services/allstarfull_csv_service.py
+++ dbSetup/services/allstarfull_csv_service.py
@@ -7,6 +7,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+def model_to_dict(model):
+    """
+    Convert a SQLAlchemy model instance to a dictionary.
+    """
+    return {
+        column.name: getattr(model, column.name) for column in model.__table__.columns
+    }
+
+
 def upload_allstarfull_csv():
     # Define the path to the CSV file
     base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +28,7 @@ def upload_allstarfull_csv():
 
     # Process the CSV file
     try:
-        update_allstarfull_from_csv(csv_file_path)
+        print(update_allstarfull_from_csv(csv_file_path))
         print("File processed successfully")
     except Exception as e:
         print(f"Error: {str(e)}")
@@ -53,11 +62,17 @@ def update_allstarfull_from_csv(file_path):
             teamID = row["teamID"] or None
             gameID = row["gameID"] or None
             lgID = row["lgID"] or None
-            GP = row.get("GP", None)
-            startingPos = row.get("startingPos", None)
+            GP = int(row["GP"]) if row["GP"] else None
+            startingPos = int(row["startingPos"]) if row["startingPos"] else None
+
+            # Debug prints
+            print(
+                f"Processing row: playerID={playerID}, yearID={yearID}, teamID={teamID}, gameID={gameID}, lgID={lgID}, GP={GP}, startingPos={startingPos}"
+            )
 
             # Check if playerID exists in the people table
             player_exists = session.query(People).filter_by(playerID=playerID).first()
+
             if not player_exists:
                 print(
                     f"playerID {playerID} does not exist in the people table. Skipping row."
@@ -65,6 +80,7 @@ def update_allstarfull_from_csv(file_path):
                 continue
 
             lg_exists = session.query(Leagues).filter_by(lgID=lgID).first()
+
             if not lg_exists:
                 print(f"lgID {lgID} does not exist in the leagues table. Skipping row.")
                 continue
@@ -75,9 +91,8 @@ def update_allstarfull_from_csv(file_path):
                 .filter_by(
                     playerID=playerID,
                     yearID=yearID,
+                    lgID=lgID,
                     teamID=teamID,
-                    gameID=gameID,
-                    startingPos=startingPos,
                 )
                 .first()
             )
