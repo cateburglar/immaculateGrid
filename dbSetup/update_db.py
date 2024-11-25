152d9ef26e370756063244300e5708790b557d9f
Brendon <brendonnewton0@gmail.com>
2024-11-25 08:39:35 -0600

    implementing multiprocessing to speed things up

--- dbSetup/update_db.py
+++ dbSetup/update_db.py
@@ -1,35 +1,45 @@
 import argparse
 import inspect
-import signal
+import io
+import multiprocessing
+from contextlib import redirect_stderr, redirect_stdout
 
 import services
 
 
-def timeout_handler(signum, frame):
-    raise TimeoutError("Function call timed out")
+def wrapper(result_queue, func, args, kwargs):
+    """Wrapper function to call the target function and put the result in a queue."""
+    f = io.StringIO()
+    with redirect_stdout(f), redirect_stderr(f):
+        result = func(*args, **kwargs)
+    result_queue.put((result, f.getvalue()))
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Update database.")
-    parser.add_argument(
-        "tables",
-        metavar="T",
-        type=str,
-        nargs="*",
-        help="Names of the tables to update (e.g., people teams allstarfull schools). If no tables are provided, all will be updated.",
+def run_with_timeout(func, args=(), kwargs={}, timeout=10):
+    """
+    Runs a function with a timeout.
+
+    :param func: The function to execute.
+    :param args: Positional arguments for the function.
+    :param kwargs: Keyword arguments for the function.
+    :param timeout: Timeout in seconds.
+    :return: The result of the function or None if it times out.
+    """
+    result_queue = multiprocessing.Queue()
+
+    process = multiprocessing.Process(
+        target=wrapper, args=(result_queue, func, args, kwargs)
     )
-    args = parser.parse_args()
+    process.start()
+    process.join(timeout)
 
-    # If no tables are specified, call all functions in services
-    tables_to_update = (
-        args.tables
-        if (args.tables and args.tables != ["ci"])
-        else get_all_service_functions()
-    )
-    isCI = True if args.tables == "test" else False
-
-    # Execute updates
-    update_tables(tables_to_update, isCI)
+    if process.is_alive():
+        process.terminate()
+        process.join()
+        return (None,)  # Functions have no return value
+    else:
+        # If completed, retrieve the result
+        return result_queue.get()
 
 
 def get_all_service_functions():
@@ -41,25 +51,46 @@ def get_all_service_functions():
     ]
 
 
-def update_tables(tables, isCI):
+def update_table(table):
+    func_name = f"upload_{table}_csv"
+    if hasattr(services, func_name):
+        func = getattr(services, func_name)
+
+        f = io.StringIO()
+        with redirect_stdout(f), redirect_stderr(f):
+            func()  # Call function
+        print(f.getvalue())
+    else:
+        print(f"Unknown table: {table}")
+
+
+def update_tables(tables):
+    if tables == ["0"]:
+        tables = get_all_service_functions()
+
+    processes = []
     for table in tables:
-        func_name = f"upload_{table}_csv"
-        if hasattr(services, func_name):
-            func = getattr(services, func_name)
-            if isCI:
-                signal.signal(signal.SIGALRM, timeout_handler)
-                signal.alarm(10)
-                try:
-                    func()  # Call function in test mode
-                except TimeoutError:
-                    print(f"Update for table {table} timed out")
-                finally:
-                    signal.alarm(0)  # Disable the alarm
-            else:
-                func()  # Call function normally
-        else:
-            print(f"Unknown table: {table}")
+        p = multiprocessing.Process(target=update_table, args=(table))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Update database tables.")
+    parser.add_argument(
+        "tables",
+        metavar="T",
+        type=str,
+        nargs="*",
+        help="Names of the tables to update (e.g., people teams allstarfull schools). If no tables are provided, all will be updated.",
+    )
+    args = parser.parse_args()
+
+    # If no tables are specified, call all functions in services
+    tables_to_update = args.tables if args.tables else ["0"]
+
+    # Execute updates
+    update_tables(tables_to_update)
