76b0a029942bebc3b220d8e283308f5b43830005
Brendon <brendonnewton0@gmail.com>
2024-11-25 22:18:57 -0600

    using batch processing with people to avoid deadlocks

--- dbSetup/services/people_csv_service.py
+++ dbSetup/services/people_csv_service.py
@@ -2,7 +2,7 @@ import csv
 
 from csi3335f2024 import mysql
 from models import People
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import OperationalError
 from utils import create_enginestr_from_values, create_session_from_str, get_csv_path
 
 
@@ -22,20 +22,28 @@ def upload_people_csv():
 
 
 def update_people_from_csv(file_path):
+    counts = {"updated_rows": 0}
     try:
         with open(file_path, newline="") as csvfile:
             reader = csv.DictReader(csvfile)
             session = create_session_from_str(create_enginestr_from_values(mysql))
-            counts = {"updated_rows": 0}
-            # Process rows
+            batch_size = (
+                500  # Committing in batches causes less locks to be messed with
+            )
+            rows_processed = 0
+
             for row in reader:
                 process_row(row, session, counts)
-            # Commit updates
-            session.commit()
-    except (csv.Error, SQLAlchemyError) as e:
-        session.rollback()
-        raise RuntimeError(f"Error updating people from CSV: {str(e)}")
+                rows_processed += 1
+
+                if rows_processed % batch_size == 0:
+                    session.commit()  # Commit after processing a batch of rows
+
+            session.commit()  # Commit any remaining rows
+            print(f"Committed remaining rows")
+
     except Exception as e:
+        session.rollback()
         raise RuntimeError(f"Unexpected error: {str(e)}")
 
     return counts
@@ -43,7 +51,7 @@ def update_people_from_csv(file_path):
 
 def process_row(row, session, counts):
     try:
-        person = People(
+        entry = People(
             playerID=row["playerID"],
             birthYear=(row["birthYear"] or None),
             birthMonth=(row["birthMonth"] or None),
@@ -67,7 +75,16 @@ def process_row(row, session, counts):
             debutDate=(row["debut"] or None),
             finalGameDate=(row["finalGame"] or None),
         )
-        counts["updated_rows"] += 1
-        session.merge(person)  # Upsert row
-    except SQLAlchemyError as e:
-        raise RuntimeError(f"Error processing row: {str(e)}")
+
+        with session.no_autoflush:
+            session.merge(entry)
+            session.commit()
+            counts["updated_rows"] += 1
+
+    except OperationalError as e:
+        session.rollback()
+        if "Deadlock found when trying to get lock" in str(e):
+            print(f"Deadlock detected for row {row['playerID']}. Retrying...")
+            process_row(row, session, counts)
+        else:
+            raise RuntimeError(f"Error processing row: {str(e)}")
