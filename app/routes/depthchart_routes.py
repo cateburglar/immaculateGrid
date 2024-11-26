4e45f1e9ec6b2a95d5d96b1a0b34ab0cd16aaceb
cateburglar <catherinearling@gmail.com>
2024-11-26 00:24:04 -0600

    input validation and small changes

--- app/routes/depthchart_routes.py
+++ app/routes/depthchart_routes.py
@@ -14,16 +14,22 @@ def depth_chart():
     if form.validate_on_submit():
         teamName = form.teamName.data
         year = form.yearID.data
+        flag=False
 
         #find teamID of given team name
         team = db.session.query(Teams).filter_by(team_name=teamName).first()
         if not team:
+            flag=True
             flash("team not found", "error")
+        if year < 1871 or year > 2023:
+            flag=True
+            flash("year must be >= 1871 and <= 2023", "error")
+        if flag:
             return render_template("depth_chart.html", form=form)
         
         team_ID = team.teamID
         # Query the database for the player's batting stats for the given yearID and teamID
-        #THIS WILL BE REPLACED BY BATTING STATS EVENTUALLY
+        #THIS WILL BE REPLACED BY BATTING STATS EVENTUALLY TODO
         players = db.session.query(Fielding).filter_by(yearID=year, teamID=team_ID).all()
 
         # Group players by position
@@ -32,11 +38,11 @@ def depth_chart():
             position = player.position
             if position not in depth_chart_data:
                 depth_chart_data[position] = []
-            #THIS WILL CONTAIN BATTING STATS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            #THIS WILL CONTAIN BATTING STATS TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             depth_chart_data[position].append(player)
 
 
-        # Query the database for batting leaders---- cant do this yet...!!!!!!!!!!!!!!!!!
+        # Query the database for batting leaders---- TODO cant do this yet...!!!!!!!!!!!!!!!!!
         '''
         batting_leaders = db.session.query(Batting).filter_by(yearID=year, teamID=team_ID).order_by(
             Batting.homeRuns.desc(),
@@ -45,7 +51,7 @@ def depth_chart():
         ).limit(5).all()  # Adjust limit as needed
         '''
 
-        # Query the database for pitching leaders ----- WILL BE MODDED !!!!!!!!!!!!!!!
+        # Query the database for pitching leaders ----- TODO WILL BE MODDED !!!!!!!!!!!!!!!
         pitching_leaders = db.session.query(
             Pitching.playerID,
             Pitching.p_W,
@@ -63,7 +69,7 @@ def depth_chart():
              yearID=year, 
              teamID=teamName, 
              depth_chart_data=depth_chart_data,
-             #batting_leaders=batting_leaders,
+             #batting_leaders=batting_leaders, TODO
              pitching_leaders=pitching_leaders
         )
 
