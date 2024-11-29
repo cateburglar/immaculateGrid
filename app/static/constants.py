a9eafaa2ebeafa063718825f557fe41335a668e1
Brendon <brendonnewton0@gmail.com>
2024-11-28 19:23:06 -0600

    Chaning option names for clarity

--- app/static/constants.py
+++ app/static/constants.py
@@ -1,13 +1,13 @@
 OPTION_GROUPS = {
     "Career Options": {
-        "avg_career": "AVG Career",
-        "era_career": "ERA Career",
-        "wins_career": "Wins Career",
-        "k_career": "K Career",
-        "hits_career": "Hits Career",
-        "hr_career": "HR Career",
-        "save_career": "Save Career",
-        "war_career": "WAR Career",
+        "avg_career": "AVG Career Greater Than",
+        "era_career": "ERA Career Less Than",
+        "wins_career": "Wins Career More Than",
+        "k_career": "K Career More Than",
+        "hits_career": "Hits Career More Than",
+        "hr_career": "HR Career More Than",
+        "save_career": "Save Career More Than",
+        "war_career": "WAR Career More Than",
     },
     "Season Options": {
         "avg_season": "AVG Season",
@@ -199,15 +199,15 @@ TEAM_MAPPINGS = [
 TEAM_MAPPINGS = {team: team for team in TEAM_MAPPINGS}
 
 APPEARANCES_MAPPING = {
-    "P": "G_p",       # Games as pitcher
-    "C": "G_c",       # Games as catcher
-    "1B": "G_1b",     # Games as first baseman
-    "2B": "G_2b",     # Games as second baseman
-    "3B": "G_3b",     # Games as third baseman
-    "SS": "G_ss",     # Games as shortstop
-    "LF": "G_lf",     # Games as left fielder
-    "CF": "G_cf",     # Games as center fielder
-    "RF": "G_rf",     # Games as right fielder
-    "OF": "G_of",     # Games as outfielder
-    "DH": "G_dh",     # Games as designated hitter
+    "P": "G_p",  # Games as pitcher
+    "C": "G_c",  # Games as catcher
+    "1B": "G_1b",  # Games as first baseman
+    "2B": "G_2b",  # Games as second baseman
+    "3B": "G_3b",  # Games as third baseman
+    "SS": "G_ss",  # Games as shortstop
+    "LF": "G_lf",  # Games as left fielder
+    "CF": "G_cf",  # Games as center fielder
+    "RF": "G_rf",  # Games as right fielder
+    "OF": "G_of",  # Games as outfielder
+    "DH": "G_dh",  # Games as designated hitter
 }
