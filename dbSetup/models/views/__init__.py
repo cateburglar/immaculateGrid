6a361f9d565a46af60ebf28fca3f3f1898d5b73e
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-12-01 09:53:15 -0600

    psv orm update

--- dbSetup/models/views/__init__.py
+++ dbSetup/models/views/__init__.py
@@ -22,13 +22,14 @@ class PitchingStatsView(Base):
     nameFirst = Column(String(255), nullable = True)
     nameLast = Column(String(255), nullable = True)
     nameGiven = Column(String(255), nullable = True)
-    age = Column(Integer, nullable = True)
 
     # Traditional pitching stats
     p_G = Column(SmallInteger, nullable = True)
     p_GS = Column(SmallInteger, nullable = True)
+    p_ERA = Column(Float, nullable = True)
 
     # Advanced pitching metrics
+    age = Column(Integer, nullable = True) # Derive from People birth and death info
     p_IP = Column(Float, nullable = True) # Derived from p_IPouts
     p_K_percent = Column(Float, nullable = True) # Strikeout Percentage
     p_BB_percent = Column(Float, nullable = True) # Walk Percentage
@@ -37,7 +38,6 @@ class PitchingStatsView(Base):
     p_LOB_percent = Column(Float, nullable = True) # Left On Base Percentage
     p_GB_percent= Column(Float, nullable = True) # Ground Ball Percentage
     p_HR_div_FB = Column(Float, nullable = True) # HR per Fly Ball
-    p_ERA = Column(Float, nullable = True) # Earned Run Average
     p_FIP = Column(Float, nullable = True) # Fielding Independent Pitching
     p_XFIP = Column(Float, nullable = True) # Expected Fielding Independent Pitching
     p_WAR = Column(Float, nullable = True) # Wins Above Replacement
