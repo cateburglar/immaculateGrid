a87e8c11baa4acdf8bb179263f5f2f0c332654b1
cateburglar <catherinearling@gmail.com>
2024-12-01 21:06:07 -0600

    moved it

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -26,6 +26,78 @@ logger.addHandler(file_handler)
 
 home_routes = Blueprint("home_routes", __name__, template_folder="templates")
 
+@home_routes.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
+        if not existing_user:
+            # Hash the password
+            hashed_password = generate_password_hash(
+                form.password.data, method="scrypt"
+            )
+
+            # Create a new User instance
+            new_user = User(
+                nameFirst=form.nameFirst.data,
+                nameLast=form.nameLast.data,
+                username=form.username.data,
+                password=hashed_password,
+            )
+
+            # Add the new user
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Log account creation
+            logger.info(f"Account created: {new_user.username}")
+
+            # Show a successs message and redirect to login
+            flash("Account created successfully!", "success")
+            login_user(new_user, remember=False)
+
+            # Log login
+            logger.info(f"Successful login: {new_user.username}")
+
+            flash(f"Welcome, {new_user.username}!", "success")
+            return redirect(url_for("home_routes.home"))
+
+        else:
+            logger.info(f"Duplicate account creation attempt for {form.username.data}")
+            flash("Username is already taken, please try again", "danger")
+    return render_template("signup.html", title="Sign Up", form=form)
+
+
+# /login
+@home_routes.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Retrieve the user from the database
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # Verify if the user exists and the password is correct
+        if user and check_password_hash(user.password, form.password.data):
+            # Log in the user and manage 'remember me' option
+            login_user(user, remember=form.remember_me.data)
+            logger.info(f"Successful login: {user.username}")
+            session["username"] = user.username
+            flash(f"Welcome, {user.nameFirst} {user.nameLast}!", "success")
+            return redirect(url_for("home_routes.home"))
+
+        logger.info(f"Failed login attempt: {form.username.data}")
+        flash("Invalid username or password", "danger")
+
+    return render_template("login.html", title="Sign In", form=form)
+
+
+@home_routes.route("/logout")
+def logout():
+    logger.info(f"Logout: {session["username"]}")
+    logout_user()
+    flash("You have been logged out.", "info")
+    return redirect(url_for("home_routes.login"))
+
 
 # /
 @home_routes.route("/", methods=["GET", "POST"])
@@ -99,76 +171,3 @@ def home():
         )
 
     return render_template("depth_chart.html", form=form)
-
-
-@home_routes.route("/signup", methods=["GET", "POST"])
-def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        existing_user = User.query.filter_by(username=form.username.data).first()
-        if not existing_user:
-            # Hash the password
-            hashed_password = generate_password_hash(
-                form.password.data, method="scrypt"
-            )
-
-            # Create a new User instance
-            new_user = User(
-                nameFirst=form.nameFirst.data,
-                nameLast=form.nameLast.data,
-                username=form.username.data,
-                password=hashed_password,
-            )
-
-            # Add the new user
-            db.session.add(new_user)
-            db.session.commit()
-
-            # Log account creation
-            logger.info(f"Account created: {new_user.username}")
-
-            # Show a successs message and redirect to login
-            flash("Account created successfully!", "success")
-            login_user(new_user, remember=False)
-
-            # Log login
-            logger.info(f"Successful login: {new_user.username}")
-
-            flash(f"Welcome, {new_user.username}!", "success")
-            return redirect(url_for("home_routes.home"))
-
-        else:
-            logger.info(f"Duplicate account creation attempt for {form.username.data}")
-            flash("Username is already taken, please try again", "danger")
-    return render_template("signup.html", title="Sign Up", form=form)
-
-
-# /login
-@home_routes.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Retrieve the user from the database
-        user = User.query.filter_by(username=form.username.data).first()
-
-        # Verify if the user exists and the password is correct
-        if user and check_password_hash(user.password, form.password.data):
-            # Log in the user and manage 'remember me' option
-            login_user(user, remember=form.remember_me.data)
-            logger.info(f"Successful login: {user.username}")
-            session["username"] = user.username
-            flash(f"Welcome, {user.nameFirst} {user.nameLast}!", "success")
-            return redirect(url_for("home_routes.home"))
-
-        logger.info(f"Failed login attempt: {form.username.data}")
-        flash("Invalid username or password", "danger")
-
-    return render_template("login.html", title="Sign In", form=form)
-
-
-@home_routes.route("/logout")
-def logout():
-    logger.info(f"Logout: {session["username"]}")
-    logout_user()
-    flash("You have been logged out.", "info")
-    return redirect(url_for("home_routes.login"))
